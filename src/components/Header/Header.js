ec2c45b83fba8051a9835982fe6e5e526675e873
zoeIML <zoe.lisk@gmail.com>
2018-09-14 13:58:10 +1200

    header component renders

---
+++ src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+// import styles from './styles'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <View>
+                <Text>This is a header</Text>
+            </View>
+        )
+    }
+}
+
+export default Header
